1c38fd32dde970fc8f36ec70de916c05c62178a1
Erik Yang <erikyangs@gmail.com>
2016-07-29 14:36:18 +0800

    support for capital letters, multiple spaces, multiple words, new lines

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
-// Define the `shitpost` module
+var SYMBOLS = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~ \n\\';
+
+// Define the shitpost module
 var shitpost = angular.module('shitpost', []);
 
 shitpost.controller('mainController', function mainController($scope) {
+    //stores data from textarea from ng-model
     $scope.input = "";
 
     $scope.convertToShitpost = function(input) {
         if (emojisReady) {
-            if(emojiMappings[input]){
-            	return input + emojiMappings[input].char;
-            }
-            else{
-            	return input;
-            }
+            var wordArray = createWordArray(input);
+            return wordArrayToShitpost(wordArray);
         }
     }
 });
+
+function createWordArray(input) {
+    var output = [];
+    var currentWord = "";
+    for (var i = 0; i < input.length; i++) {
+        //if symbol present
+        if (SYMBOLS.indexOf(input[i]) > -1) {
+            if (currentWord != "") {
+                output.push(currentWord);
+                currentWord = "";
+            }
+            output.push(input[i]);
+        } else {
+            currentWord += input[i];
+        }
+    }
+    //push last word
+    if (currentWord != "") {
+        output.push(currentWord);
+    }
+    return output;
+}
+
+function wordArrayToShitpost(wordArray) {
+	var output = "";
+    for (var i = 0; i < wordArray.length; i++) {
+    	word = wordArray[i].toLowerCase();
+    	output += word;
+
+        var emoji = emojiMappings[word];        
+        if (emoji) {
+             output += " " + emoji.char;
+        }
+    }
+    return output;
+}
+
+//inclusive min, exclusive max
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 //emoji translation
 //emojilib: https://raw.githubusercontent.com/muan/emojilib/master/emojis.json
