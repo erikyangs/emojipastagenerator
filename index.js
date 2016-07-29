345be48ea0282d114bfff5577864d7fc22294e8c
Erik Yang <erikyangs@gmail.com>
2016-07-29 10:38:00 +0800

    one word functionality

--- index.js
+++ index.js
@@ -2,5 +2,31 @@
 var shitpost = angular.module('shitpost', []);
 
 shitpost.controller('mainController', function mainController($scope) {
-  $scope.test = "";
+    $scope.input = "";
+
+    $scope.convertToShitpost = function(input) {
+        if (emojisReady) {
+            if(emojiMappings[input]){
+            	return input + emojiMappings[input].char;
+            }
+            else{
+            	return input;
+            }
+        }
+    }
 });
+
+//emoji translation
+//emojilib: https://raw.githubusercontent.com/muan/emojilib/master/emojis.json
+var emojisReady = false;
+var emojiMappings;
+$(document).ready(function() {
+    $.get("https://raw.githubusercontent.com/muan/emojilib/master/emojis.json", function(data, status) {
+        parseJSON(data);
+        emojisReady = true;
+    });
+});
+
+function parseJSON(data) {
+    emojiMappings = JSON && JSON.parse(data) || $.parseJSON(data);
+}
